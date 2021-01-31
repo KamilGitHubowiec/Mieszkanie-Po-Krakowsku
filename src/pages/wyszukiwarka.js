f765fe326458a8097f97bdc3f5816adcb322d3ae
Kamil <hantsky123@gmail.com>
2021-02-01 00:03:42 +0100

    Filter and sort is working altogether but there is only one filter

--- src/pages/wyszukiwarka.js
+++ src/pages/wyszukiwarka.js
@@ -20,7 +20,7 @@ const Wyszukiwarka = () => {
             pokoje
             ogrzewanie
             pitro
-            createdAt(formatString: "DD MM YYYY")
+            createdAt(formatString: "MM DD YYYY")
             powierzchniaCalkowitaM2
             zdjecia {
               fluid {
@@ -36,13 +36,9 @@ const Wyszukiwarka = () => {
   `)
 
   const nieruchomosci = data.allContentfulNieruchomosc.edges
-  const [nieruchomosciArr, setNieruchomosciArr] = useState([...nieruchomosci])
-
-  const sortArray = type => {
-    const sorted = [...nieruchomosci].sort((a, b) => b.node[type] - a.node[type])
-    setNieruchomosciArr(sorted)
-    console.log(nieruchomosci[0].node.createdAt)
-  }
+  const [filteredNieruchomosciArr, setFilteredNieruchomosciArr] = useState(nieruchomosci)
+  const [sortOption, setSortOption] = useState('createdAt')
+  const [filters, setFilters] = useState('')
 
   const insertSpaceAfterThirdChar = num => {
     let numStr = num.toString()
@@ -58,15 +54,73 @@ const Wyszukiwarka = () => {
     }
   }
 
+  const sortArray = type => {
+    setSortOption(type)
+    let sorted = []
+
+    if (type === 'createdAt') {
+      sorted = [...filteredNieruchomosciArr].sort((a, b) => {
+        const c = new Date(a.node[type])
+        const d = new Date(b.node[type])
+        return c - d
+      })
+    } else {
+      sorted = [...filteredNieruchomosciArr].sort((a, b) => b.node[type] - a.node[type])
+    }
+    setFilteredNieruchomosciArr(sorted)
+  }
+
+  const filterArray = e => {
+    e.preventDefault()
+    const filtered = nieruchomosci.filter(cur => {
+      if (
+        cur.node['ulica'].toLowerCase().includes(filters.toLowerCase()) ||
+        filters.trim() === ''
+      ) {
+        return cur
+      }
+    })
+    setFilteredNieruchomosciArr(
+      [...filtered].sort((a, b) => b.node[sortOption] - a.node[sortOption])
+    )
+  }
+
+  const addFilter = filter => {
+    setFilters(filter)
+  }
+
   return (
     <Layout>
       <Head title="Wyszukiwarka" />
-      <div className={wyszukiwarkaStyles.filterBar}></div>
+      <div className={wyszukiwarkaStyles.filterBar}>
+        <form onSubmit={e => filterArray(e)}>
+          <input
+            type="text"
+            placeholder="Szukaj po miastach i ulicach"
+            name="ulica"
+            value={filters}
+            onChange={e => addFilter(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Cena"
+            value={filters}
+            onChange={e => addFilter(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Cena"
+            value={filters}
+            onChange={e => addFilter(e.target.value)}
+          />
+          <input type="submit" value="Szukaj" />
+        </form>
+      </div>
       <div className={wyszukiwarkaStyles.body}>
         <div className={wyszukiwarkaStyles.header}>Wyniki wyszukiwania</div>
 
         <div className={wyszukiwarkaStyles.sortBar}>
-          <div>Ilość nieruchmości: {nieruchomosci.length}</div>
+          <div>Ilość nieruchmości: {filteredNieruchomosciArr.length}</div>
           <div className={wyszukiwarkaStyles.dropdownListSort}>
             {/* <button className={wyszukiwarkaStyles.sortButton}>
               Sortuj wg{' '}
@@ -80,17 +134,17 @@ const Wyszukiwarka = () => {
             >
               <option value="createdAt">Nowości</option>
               <option value="cena">Najwyższa cena</option>
-              {/* <option value={{ sortBy: 'cena', order: 'DESC' }}>Najwyższa cena</option> */}
               <option value="powierzchniaCalkowitaM2">Największy metraż</option>
+              {/* <option value={{ sortBy: 'cena', order: 'DESC' }}>Najwyższa cena</option> */}
               {/* <option value="powierzchniaCalkowitaM2">Największy metraż</option> */}
             </select>
           </div>
         </div>
 
         <div className={wyszukiwarkaStyles.results}>
-          {nieruchomosciArr.map(edge => {
+          {filteredNieruchomosciArr.map(edge => {
             return (
-              <div className={wyszukiwarkaStyles.result}>
+              <div className={wyszukiwarkaStyles.result} key={edge.node.id}>
                 <div className={wyszukiwarkaStyles.images}>
                   {edge.node.zdjecia ? (
                     <Link
